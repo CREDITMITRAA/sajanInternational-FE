867a8be036f24c568a9dce9b844215890fe89761
CREDITMITRAA <developers@creditmitra.in>
2025-07-15 15:24:55 +0530

    added base url backend for produdction server

--- src/api/index.js
+++ src/api/index.js
@@ -1,2 +1,3 @@
 // export const BASE_URL_SAJAN = 'http://localhost:3002' ; //localhost for development
 export const BASE_URL_SAJAN = 'https://api.uat.sajaninternational.com'; //Development server
+// export const BASE_URL_SAJAN = 'https://api.uat.sajaninternational.com'; //Production server
